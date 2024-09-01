2ca2b611bd4434623cb1cba38c7f64c335bb0690
Gustavo Facchinetti <88977361+Knucklesx@users.noreply.github.com>
2024-09-01 09:09:23 -0300

    LoginPage css (#2)

--- adaptedfront/components/login/loginForm.tsx
+++ adaptedfront/components/login/loginForm.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export default function LoginForm() {
-	const { register, handleSubmit } = useForm({
+	const [loading, setLoading] = useState(false);
+	const { register, handleSubmit, watch } = useForm({
 		defaultValues: {
 			username: "",
 			password: "",
@@ -12,8 +14,10 @@ export default function LoginForm() {
 	});
 	const onSubmit = async (data: any) => {
 		try {
+			setLoading(true);
 			const res = await axios.post("http://localhost:3001/auth", data);
 			console.log("res", res.data);
+			setLoading(false);
 		} catch (error: any) {
 			if ((error as any).response && (error as any).response.status === 401) {
 				console.error("Unauthorized: Invalid username or password");
@@ -22,17 +26,52 @@ export default function LoginForm() {
 			}
 		}
 	};
+
+	const isButtonDisabled = () => {
+		const username = watch("username");
+		const password = watch("password");
+		return loading || !username || !password;
+	};
+
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<div>
-				<label htmlFor="username">Username</label>
-				<input type="text" {...register("username")} />
+			<div className="relative w-full mb-3">
+				<label
+					htmlFor="username"
+					className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+				>
+					Username
+				</label>
+				<input
+					type="text"
+					{...register("username")}
+					className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+				/>
 			</div>
-			<div>
-				<label htmlFor="password">Senha</label>
-				<input type="password" {...register("password")} />
+			<div className="relative w-full mb-3">
+				<label
+					htmlFor="password"
+					className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+				>
+					Senha
+				</label>
+				<input
+					type="password"
+					{...register("password")}
+					className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+				/>
 			</div>
-			<button type="submit">Entrar</button>
+			<button
+				className={`text-white text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150 ${
+					isButtonDisabled()
+						? "bg-gray-400 cursor-not-allowed"
+						: "bg-black active:bg-black"
+				}`}
+				type="submit"
+				disabled={isButtonDisabled()}
+			>
+				Entrar
+			</button>
 		</form>
 	);
 }
