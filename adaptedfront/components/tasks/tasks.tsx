7d8a1f6e3147e13107fa5cface456b082efce9b3
Gustavo Facchinetti <gcfacchi@gmail.com>
2024-09-03 22:45:00 -0300

    tasks

--- adaptedfront/components/tasks/tasks.tsx
+++ adaptedfront/components/tasks/tasks.tsx
@@ -1,6 +1,9 @@
 "use client";
+import axios from "axios";
+import { format } from "date-fns";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { MyButton } from "../button/button";
 import { fetchApi } from "../helper/fetchApi";
 import { ProjectsReponse } from "../helper/projects.dto";
 
@@ -30,12 +33,33 @@ export default function TaskPage({ id }: { id: number }) {
 
 		loadProjects();
 	}, [session, status, id]);
+
+	const handleDeleteTask = async (taskId: number) => {
+		const tt = Object.values(projects.data);
+		console.log(tt);
+		try {
+			await axios.delete(`http://localhost:3001/tasks/${taskId}`, {
+				headers: {
+					Authorization: `Bearer ${session?.access_token}`,
+				},
+			});
+			setProjects((prevProjects) => ({
+				...prevProjects,
+				data: {
+					...prevProjects.data,
+					tasks: Object.values(tt[3]).filter((task) => task.id !== taskId),
+				},
+			}));
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
 	if (loading) {
 		return <div>Loading...</div>;
 	}
 
 	const myProj = Object.values(projects.data);
-	console.log("ppp", myProj);
 	const myProjName = typeof myProj[1] === "string" ? myProj[1] : "";
 
 	return (
@@ -51,16 +75,47 @@ export default function TaskPage({ id }: { id: number }) {
 						{Object.values(projects).map((p) => (
 							<li key={p.id} className="mb-2">
 								<div className="p-4 bg-gray-100 rounded">
-									{/* <h2 className="text-xl font-semibold">{p.name}</h2> */}
 									<div className="flex">
 										<p className="font-bold text-xl">Descrição:</p>
 										<p className="text-lg"> {p.description}</p>
 									</div>
 									<div>
 										{p.tasks.map((t: any) => (
-											<div key={t.id} className="p-4 bg-gray-200 rounded">
-												<h2 className="text-xl font-semibold">{t.name}</h2>
-												<p>{t.description}</p>
+											<div key={t.id} className="p-4 bg-gray-200 rounded my-10">
+												<div className="flex justify-between items-center">
+													<div>
+														<div className="flex">
+															<p className="text-xl font-bold">
+																Nome da tarefa:
+															</p>
+															<h2 className="text-xl font-semibold mx-1">
+																{t.name}
+															</h2>
+														</div>
+														<div className="flex">
+															<p className="font-bold">Descrição:</p>
+															<p className="mx-1">{t.description}</p>
+														</div>
+														<div className="flex">
+															<p className="font-bold">Data de Vencimento:</p>
+															<p className="mx-1">
+																{format(
+																	new Date(t.data_de_vencimento),
+																	"dd/MM/yyyy"
+																)}
+															</p>
+														</div>
+														<div className="flex">
+															<p className="font-bold">Status:</p>
+															<p className="mx-1">{t.status}</p>
+														</div>
+													</div>
+													<MyButton
+														text="Deletar"
+														onClick={() => handleDeleteTask(t.id)}
+														className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+													/>
+												</div>
 											</div>
 										))}
 									</div>
